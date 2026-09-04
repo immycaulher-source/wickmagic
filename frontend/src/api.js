3a90e7c08bba4efcdadae0460a6f1ff33fbfed92
immy <yue5h67kl@paratrabajar.xyz>
2026-09-05 03:27:19 +0545

    changed api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,24 +1,22 @@
-import axios from 'axios'
-import {ACCESS_TOKEN} from './constants'
+import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
+
+// Clean up any double slashes or undefined values
+const API_URL = import.meta.env.VITE_API_URL || 'https://wickmagic.onrender.com';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://wickmagic.onrender.com'
-})
+  baseURL: API_URL.replace(/\/$/, ''), // Removes trailing slash if present
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token){
-            config.headers.Authorization = `Bearer ${token}`
-        }
-
-        return config
-
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-export default api
-
+export default api;
