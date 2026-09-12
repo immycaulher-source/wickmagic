8055e4fe2f1056a454ab3be094ee4be8b3a344f5
immy <yue5h67kl@paratrabajar.xyz>
2026-09-12 15:03:17 +0545

    db.sqlite3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,10 +100,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
     )
 }
+
 
 
 # Password validation
